0398c9d165c492b4402d516dde7d2ca97d16100a
Eduardo Peredo Rivero <jrperedo@gmail.com>
2025-11-23 00:09:29 -0500

    only include org. units within the users scope in filter by level/group

--- src/org-unit-select/OrgUnitSelectByGroup.component.js
+++ src/org-unit-select/OrgUnitSelectByGroup.component.js
@@ -23,6 +23,7 @@ class OrgUnitSelectByGroup extends React.Component {
             selection: undefined,
         };
         this.groupCache = {};
+        this.orgUnitsInHierarchy = undefined;
 
         this.addToSelection = addToSelection.bind(this);
         this.removeFromSelection = removeFromSelection.bind(this);
@@ -85,9 +86,17 @@ class OrgUnitSelectByGroup extends React.Component {
                                   levelsToFilter.includes(orgUnit.level)
                               )
                             : organisationUnits;
-                        this.groupCache[groupId] = filterOrgUnits;
+
                         // Make a copy of the returned array to ensure that the cache won't be modified from elsewhere
-                        resolve(filterOrgUnits);
+                        const excludeOrgUnits = this.state.orgUnitsInHierarchy
+                            ? filterOrgUnits.filter(orgUnit =>
+                                  this.state.orgUnitsInHierarchy.has(orgUnit.id)
+                              )
+                            : filterOrgUnits;
+
+                        this.groupCache[groupId] = excludeOrgUnits;
+
+                        resolve(excludeOrgUnits);
                     })
                     .catch(err => {
                         this.setState({ loading: false });
@@ -113,6 +122,24 @@ class OrgUnitSelectByGroup extends React.Component {
         // Material-UI SelectField will change height depending on whether or not it has a value
         return renderDropdown.call(this, menuItems, label);
     }
+
+    componentDidMount() {
+        const { api } = this.context;
+        if (this.props.withinUserHierarchyInFilters) {
+            api.get("/organisationUnits/", {
+                paging: false,
+                fields: "id",
+                withinUserHierarchy: this.props.withinUserHierarchyInFilters,
+            })
+                .getData()
+                .then(response => {
+                    if (response.organisationUnits.length > 0) {
+                        const orgUnitIdsSet = new Set(response.organisationUnits.map(ou => ou.id));
+                        this.setState({ orgUnitsInHierarchy: orgUnitIdsSet });
+                    }
+                });
+        }
+    }
 }
 
 OrgUnitSelectByGroup.propTypes = {
@@ -134,7 +161,7 @@ OrgUnitSelectByGroup.propTypes = {
     // If currentRoot is set, only org units that are descendants of the
     // current root org unit will be added to or removed from the selection
     currentRoot: PropTypes.object,
-
+    withinUserHierarchyInFilters: PropTypes.bool,
     // TODO: Add group cache prop?
 };
 
