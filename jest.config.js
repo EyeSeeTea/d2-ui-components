f7d681bac3bc094a315d55ab8e7514419b43f6fe
Arnau Sanchez <pyarnau@gmail.com>
2026-04-21 13:31:19 +0200

    chore: add js to jest extensions

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
         "^.+\\.tsx?$": "ts-jest",
     },
     testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-    moduleFileExtensions: ["ts", "tsx", "json", "node"],
+    moduleFileExtensions: ["js", "ts", "tsx", "json", "node"],
     transformIgnorePatterns: [],
     snapshotSerializers: ["enzyme-to-json/serializer"],
     setupFilesAfterEnv: ["<rootDir>/setupEnzyme.ts"],
